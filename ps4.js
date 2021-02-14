99384b032ba0d0fbe1e0de713b65f7179409107d
Emmi Carr <escarr@umich.edu>
2021-02-14 17:07:25 -0500

    first commit

---
+++ ps4.js
@@ -0,0 +1,55 @@
+const addIngred = document.getElementById("addIngredient");
+
+addIngred.addEventListener("click", () => {
+    // add ingredient to list
+    const ingredientsList = document.getElementById("ingredients_list");
+    const newIngredient = document.createElement("li");
+    const ingredientName = document.getElementById("iname").value;
+    const ingredientAmount = document.getElementById("iamount").value;
+    const ingredientUnit = document.getElementById("iunit").value;
+    const ingredientText = ingredientName + ': ' + ingredientAmount + ' ' + ingredientUnit
+    newIngredient.appendChild(document.createTextNode(ingredientText));
+    ingredientsList.appendChild(newIngredient);
+    // clear inputs
+    document.getElementById("iname").value = "";
+    document.getElementById("iamount").value = "";
+    document.getElementById("iunit").value = "";
+
+});
+
+
+const addInstruc = document.getElementById("addInstruction");
+
+addInstruc.addEventListener("click", () => { 
+  // add instruction to list
+  const ingredientsList = document.getElementById("instructions_list");
+  const newInstruction = document.createElement("li");
+  const InstructionText = document.getElementById("instruction").value;
+  newInstruction.appendChild(document.createTextNode(InstructionText));
+  ingredientsList.appendChild(newInstruction);
+  // clear input
+  document.getElementById("instruction").value = "";
+});
+
+
+const generateRecipe = document.getElementById("generateRecipe");
+// pop up message when recipe is complete
+generateRecipe.addEventListener("click", () => {
+    alert('Congrats! You have created a new recipe! Planning on adding more functionality here in future assignments :)')
+});
+
+const buttons = [addIngred, addInstruc, generateRecipe]
+
+buttons.forEach(function (button) {
+    button.addEventListener("mouseover", () => {
+        // highlight the button upon hover
+        button.setAttribute("class", "highlight");
+    });
+
+    button.addEventListener("mouseout", () => {
+        // un-highlight the button upon hover
+        button.setAttribute("class", "");
+    });
+
+})
+
